d612d78ac044e84c2c9b5ac7ecf446bc99a90b09
KShukhrat <qayumov7298668@gmail.com>
2023-02-05 11:16:10 +0500

    changes

--- PaymentStripeDjango/settings.py
+++ PaymentStripeDjango/settings.py
@@ -121,4 +121,7 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
+STRIPE_PUBLIC_KEY = ""
+STRIPE_SECRET_KEY = ""
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
